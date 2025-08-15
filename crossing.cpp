b43354e3591837513bfeeb29434348066e51facb
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-16 02:31:22 +0530

    traffic  light and color function

--- crossing.cpp
+++ crossing.cpp
@@ -4,6 +4,41 @@ using namespace std;
 
 
 
+void color()
+{
+    cout <<"\nWhat is the color of the traffic light?(red/green)\n";
+    string col;
+    cin>>col;
+    if(col=="red")
+    {
+        cout<<"\nColor had turned red, kindly check for cars on sides\n";
+        leftApproach();
+    }
+    else if(col=="green")
+    {
+        cout<<"\nGreen signal, cars are moving, wait for a while\n";
+        cout<<"\nWait for the signal to turn red\n";
+        color();
+    }
+}
+
+void trafficLights()
+{
+    cout <<"\nDid you find traffic lights being on?(yes/no)\n";
+    string lights;
+    cin>>lights;
+    if(lights=="yes")
+    {
+        cout<<"\nCheck for the color of the traffic light\n";
+        color();
+    }
+    else
+    {
+        cout<<"\nDidnt find any traffic lights, so looking for any cars approaching\n";
+        leftApproach();
+    }
+}
+
 void crossing()
 {
     cout <<"\nDid you find a crossing?(yes/no)\n";
